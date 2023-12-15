b115713c28c15c3919ae1fac05280b3f1ad1781f
mensahdaniel <mensahdaniel5858910@gmail.com>
2023-12-15 14:57:19 +0000

    if's statements problem solved

---
+++ if_statements/ifs.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+name = "Jane"
+age = 26
+print(f"Hello, {name.upper()}! You're {age} years old.")
---
+++ if_statements/problem.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+age = eval(input("whats your age? "))
+if age == 5:
+    print("go to kindergarten")
+elif age < 5:
+    print("thats good")
+else:
+    print("mooreal")
